0f09d20d992c41b936cae1105828a2b10955d369
NateFuller <fatenuller@gmail.com>
2015-10-19 21:25:00 -0400

    Removed extraneous comments from app.js, hooked up the team.js route, applied some indentation.

--- app.js
+++ app.js
@@ -8,55 +8,21 @@ var handlebars = require('express-handlebars');
 ///// Express App Setup //////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-// The express library is a simple function. When you invoke this
-// function it returns an express web application that you build from.
 var app = express();
 
-// This will set an "application variable". An application variable is
-// a variable that can be retrieved from your app later on. It is
-// simply a key/value mapping. In this case, we are mapping the key
-// 'port' to a port number. The port number will either be what you
-// set for PORT as an environment variable (google this if you do not
-// know what an evironment variable is) or port 3000.
 app.set('port', process.env.PORT || 3000);
 
-// This does the setup for handlebars. It first creates a new
-// handlebars object giving it the default layout. This indicates
-// that the default layout is called main.js in the views/layouts
-// directory. We then set the app's view engine to 'handlebars' - this
-// lets your express app know what the view engine is. We then set an
-// app variable 'view engine' to 'handlebars'. This is mostly boiler
-// plate so you need not worry about the details.
 var view = handlebars.create({ defaultLayout: 'main' });
 app.engine('handlebars', view.engine);
 app.set('view engine', 'handlebars');
 
-// This does the setup for static file serving. It uses express'
-// static middleware to look for files in /public if no other route
-// matches. We use the __dirname special variable which indicates the
-// directory this server is running in and append it to '/public'.
 app.use(express.static(__dirname + '/public'));
 
-// The `testmw` function represents out testing middleware. We use
-// this in our views to conditionally include the Mocha and Chai
-// testing framework as well as our own tests. Because this is a
-// middleware function it expects to receive the request object
-// (`req`), response object (`res`), and `next` function as arguments.
-// The `next` function is used to continue processing the request
-// with subsequent routes.
 function testmw(req, res, next) {
-  // This checks the 'env' application variable to determine if we are
-  // in "production" mode. An application is in "production" mode if
-  // it is actually deployed. This can be set by the NODE_ENV
-  // environment variable. It also checks to see if the request has
-  // given a `test` querystring parameter, such as
-  // http://localhost:3000/about?test=1. If the route has that set
-  // then showTests will be set to a "truthy" value. We can then
-  // use that in our handlebars views to conditionally include tests.
-  res.locals.showTests = app.get('env') !== 'production' &&
-                         req.query.test;
-  // Passes the request to the next route handler.
-  next();
+    res.locals.showTests = app.get('env') !== 'production' &&
+    req.query.test;
+    // Passes the request to the next route handler.
+    next();
 }
 
 // This adds our testing middleware to the express app.
@@ -66,19 +32,10 @@ app.use(testmw);
 ///// User Defined Routes ////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-//var team = require('./lib/team.js');
+var team = require('./lib/team.js');
 
 app.get('/', (req, res) => {
-    res.render("SUP");
-  // var result = team.all();
-  // if (!result.success) {
-  //   notFound404(req, res);
-  // } else {
-  //   res.render('team', {
-  //     members: result.data,
-  //     pageTestScript: '/qa/tests-team.js'
-  //   });
-  // }
+    res.render('splash');
 });
 
 app.get('/about', (req, res) => {
@@ -86,13 +43,26 @@ app.get('/about', (req, res) => {
 });
 
 //
-// A generic route that expects a user name in the request URL
+// A generic route that expects a user name with the 'user' querystring.
 // We search for it using team.one() in this single route
 // instead of creating multiple specific routes. This approach
 // might be preferred if we were to add/remove users.
 //
-app.get('/team/member', (req, res) => {
-    // TODO
+app.get('/team', (req, res) => {
+    var result;
+    // render a specific member's info if ?member exists
+    if (req.query.user)
+        result = team.one(req.query.user);
+    else
+        result = team.all();
+    if (!result.success) {
+        notFound404(req, res);
+    } else {
+        res.render('team', {
+            members: result.data,
+            pageTestScript: '/qa/tests-team.js'
+        });
+    }
 });
 
 
@@ -105,8 +75,8 @@ app.get('/team/member', (req, res) => {
 // found. We set the HTTP status code in the response object to 404.
 // We then render our views/404.handlebars view back to the client.
 function notFound404(req, res) {
-  res.status(404);
-  res.render('404');
+    res.status(404);
+    res.render('404');
 }
 
 // A middleware function that will be invoked if there is an internal
@@ -117,9 +87,9 @@ function notFound404(req, res) {
 // the response status code to 500, and render our
 // views/500.handlebars view back to the client.
 function internalServerError500(err, req, res, next) {
-  console.error(err.stack);
-  res.status(500);
-  res.render('500');
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
 }
 
 // This adds the two middleware functions as the last two middleware
@@ -137,6 +107,6 @@ app.use(internalServerError500);
 // parameter is a function that gets invoked after the application is
 // up and running.
 app.listen(app.get('port'), () => {
-  console.log('QueueUp started on http://localhost:' +
-              app.get('port') + '; press Ctrl-C to terminate');
+    console.log('QueueUp started on http://localhost:' +
+    app.get('port') + '; press Ctrl-C to terminate');
 });
