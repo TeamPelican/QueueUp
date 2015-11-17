eb8851b2c91d2e502a40850da356a9b9964c9509
NateFuller <fatenuller@gmail.com>
2015-11-17 05:51:10 -0500

    Reworked some user addition stuff, refactored changePassword, re-indented

--- routes/user.js
+++ routes/user.js
@@ -4,67 +4,65 @@ var db = require('../lib/MongoDB-user.js');
 var router = express.Router();
 
 router.post('/auth', (req,res) => {
-    var user = req.session.user;
-    if (user==='undefined'){
-        req.flash("login","Please log in");
-        res.redirect('/login');
-        return;
+  var user = req.session.user;
+  if (user==='undefined'){
+    req.flash("login","Please log in");
+    res.redirect('/login');
+    return;
+  }
+  var name = req.body.name;
+  var password = req.body.pass;
+  db.login(name , password, function(status,msg){
+    if (status===true){
+      req.session.user = {"name":name};
+      req.flash("profile",msg);
+      res.redirect('/profile');
+    }else{
+      req.flash("login",msg);
+      res.redirect('/login');
     }
-    var name = req.body.name;
-    var password = req.body.pass;
-    db.login(name , password, function(status,msg){
-        if (status===true){
-            req.session.user = {"name":name};
-            req.flash("profile",msg);
-            res.redirect('/profile');
-        }else{
-            req.flash("login",msg);
-            res.redirect('/login');
-        }
-    });
+  });
 });
 
 router.post('/add', (req,res) => {
-    var name = req.body.name;
-    var password = req.body.pass;
-    db.addUser(name,password,false,function(err){
-        if (err) {
-            // console.log(err);
-            req.flash("signup",err);
-            res.redirect('/signup');
-            return;
-        }
-        req.session.user = {"name" : name};
-        res.redirect('/profile');
-    });
+  var name = req.body.name;
+  var password = req.body.pass;
+  db.addUser(name,password,false,function(err, result){
+    if (err) {
+      req.flash("signup", err);
+      res.redirect('/signup');
+    } else {
+      req.session.user = {"name" : name};
+      res.redirect('/profile');
+    }
+  });
 });
 
 router.get('/signup', (req, res) => {
-    // TODO: everything!
-    var user = req.session.user;
-    if (user){
-        res.redirect('/profile');
-        return;
-    }
-    var message = req.flash('signup') || '';
-    res.locals.view_signup = true;
-    res.render('signup', { title : 'Sign up for QueueUp',
-    message : message});
+  var user = req.session.user;
+  if (user){
+    res.redirect('/profile');
+    return;
+  }
+  var message = req.flash('signup') || '';
+  res.locals.view_signup = true;
+  res.render('signup', { title : 'Sign up for QueueUp',
+  message : message});
 });
 
 router.get('/login', (req, res) => {
-    var user = req.session.user;
+  var user = req.session.user;
 
-    if (user) {
-        res.redirect('/profile');
-    } else {
-        var message = req.flash('login') || '';
-        res.locals.view_login = true; // for template specific css/js
-        res.render('login', {
-            title    : 'QueueUp Login',
-            message  : message
-        });
-    }
+  if (user) {
+    res.redirect('/profile');
+  } else {
+    var message = req.flash('login') || '';
+    res.locals.view_login = true; // for template specific css/js
+    res.render('login', {
+      title    : 'QueueUp Login',
+      message  : message
+    });
+  }
 });
 
 // router.post('/auth', (req, res) => {
@@ -98,31 +96,31 @@ router.get('/login', (req, res) => {
 // });
 
 router.get('/logout', function(req, res) {
-    var user = req.session.user;
+  var user = req.session.user;
 
-    // TODO: add check to see if user session exists AND is NOT already online
-    if (false) { // user && !online[user.name]
-        delete req.session.user;
-    } else {
-        // delete online[user.name];
-        delete req.session.user;
-    }
+  // TODO: add check to see if user session exists AND is NOT already online
+  if (false) { // user && !online[user.name]
+    delete req.session.user;
+  } else {
+    // delete online[user.name];
+    delete req.session.user;
+  }
 
-    // maybe add a flash method for saying "Successfully logged out?"
-    res.redirect('/user/login');
+  // maybe add a flash method for saying "Successfully logged out?"
+  res.redirect('/user/login');
 });
 
 router.get('/profile', function(req, res) {
-    var user = req.session.user;
+  var user = req.session.user;
 
-    if (!user) {
-        res.redirect('/login');
-    } else {
-        res.locals.view_profile = true;
-        res.render('profile', {
-            title: req.session.user.name + '\'s Profile'
-        });
-    }
+  if (!user) {
+    res.redirect('/login');
+  } else {
+    res.locals.view_profile = true;
+    res.render('profile', {
+      title: req.session.user.name + '\'s Profile'
+    });
+  }
 });
 
 module.exports = router;
