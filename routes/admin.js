33697cd48e08dcdf5161c650b05e94b69a0b5510
NateFuller <fatenuller@gmail.com>
2015-11-24 21:46:51 -0500

    Admin view pulls list of existing usernames.

--- routes/admin.js
+++ routes/admin.js
@@ -5,9 +5,24 @@ var router = express.Router();
 router.get('/', (req, res) => {
   var user = req.session.user;
 
+
+
   if (user) {
     if (user.admin) {
-      res.render('admin');
+      res.locals.view_admin = true;
+      db.getAllUsers(function(err, users) {
+        if (err) {
+          res.render('admin', {
+            title: 'Administration',
+            message: 'Error loading users from database!'
+          });
+        } else {
+          res.render('admin', {
+            title: 'Administration',
+            users: users
+          });
+        }
+      });
     } else {
       res.redirect('/user/profile');
     }
